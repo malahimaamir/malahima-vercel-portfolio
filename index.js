401fa52e4904216de7fa7d0c003474395ba84eed
malahimaamir <malahimaamir@gmail.com>
2024-10-19 19:02:38 +0500

    seventh

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-// Transition effect for navbar and back-to-top icon
-window.addEventListener('scroll', function() {
-    // Check if window is scrolled more than 550px
-    if (window.scrollY > 550) {
-        document.querySelector('.navbar').classList.add('solid');
-        document.querySelector('.back-to-top').classList.add('visible');
-    } else {
-        document.querySelector('.navbar').classList.remove('solid');
-        document.querySelector('.back-to-top').classList.remove('visible');
-    }
-});
+$(document).ready(function() {
 
-// Scrolling effect for Arrow icons
-document.querySelector("#scrollIcon").addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector("#about").scrollIntoView({ behavior: 'smooth' });
-});
+    // Transition effect for navbar and back-to-top icon
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 550) { 
+          $('.navbar').addClass('solid');
+          $('.back-to-top').addClass('visible'); 
+      } else {
+          $('.navbar').removeClass('solid');
+          $('.back-to-top').removeClass('visible');  
+      }
 
-document.querySelector("#nav-about").addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector("#about").scrollIntoView({ behavior: 'smooth' });
-});
+    });
 
-document.querySelector("#nav-portfolio").addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector("#portfolio").scrollIntoView({ behavior: 'smooth' });
-});
 
-document.querySelector("#nav-contact").addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector("#contact").scrollIntoView({ behavior: 'smooth' });
-});
-
-document.querySelector(".navbar-brand").addEventListener('click', function(e) {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+    // Scrolling effect for Arrow icons
+    $("#scrollIcon").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
+    $("#nav-about").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
+    $("#nav-portfolio").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#portfolio"), 1000);
+    });
+    $("#nav-contact").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#contact"), 1000);
+    });
+    $(".navbar-brand").click(function(e) {
+        e.preventDefault();
+        $.scrollTo(0, 1000);
+    });
+      
+  });
